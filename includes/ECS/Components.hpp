19ffd7a61a38803fdc16bcac6c568c907f99725d
fakizer <witcher0308@gmail.com>
2019-05-29 09:20:17 +0300

    memento mori

--- includes/ECS/Components.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "ECS.hpp"
-#include "TransformComponent.hpp"
-#include "SpriteComponent.hpp"
-#include "Controller.hpp"
-#include "ColliderComponent.hpp"
-#include "TileComponent.hpp"
-
-
